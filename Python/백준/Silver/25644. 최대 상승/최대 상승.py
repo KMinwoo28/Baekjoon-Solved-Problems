6a08b05253405bc7e90ec967f1d2ede018d29301
KMinwoo28 <166777048+KMinwoo28@users.noreply.github.com>
2024-10-03 10:05:37 +0900

    [Silver V] Title: 최대 상승, Time: 232 ms, Memory: 53244 KB -BaekjoonHub

---
+++ Python/백준/Silver/25644. 최대 상승/최대 상승.py
@@ -0,0 +1,11 @@
+# 25214와 같은 문제.
+import sys
+input = sys.stdin.readline
+N = int(input())
+A = list(map(int,input().split()))
+dp = [0] * N
+small = A[0]
+for i in range(1, N):
+    small = min(small, A[i]) # 추가 될 때 마다 최솟값 찾기
+    dp[i] = max(dp[i-1], A[i] - small) # 이전 최댓값 vs 이번에 구한거
+print(max(dp)) 
